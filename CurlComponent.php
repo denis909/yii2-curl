87d6ef9e53b6412807e812a457398b362b310707
denis909 <denis909@mail.ru>
2020-07-21 18:24:43 +0300

    n/a

---
+++ CurlComponent.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace denis909\yii;
+
+use Exception;
+
+class CurlComponent extends \yii\base\Component
+{
+
+    public $result;
+
+    public $sslVerifyPeer = false;
+
+    public $sslVerifyHost = false;
+
+    public $followLocation = true;
+
+    public function query($url, $post = null, array $options = [])
+    {
+        if ($post !== null)
+        {
+            $options[CURLOPT_POST] = true;
+            $options[CURLOPT_POSTFIELDS] = $post;
+        }
+
+        $ch = curl_init();
+
+        curl_setopt_array($ch, array_merge([
+            CURLOPT_URL => $url,
+            CURLOPT_HEADER => 0,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => $this->sslVerifyPeer,
+            CURLOPT_SSL_VERIFYHOST => $this->sslVerifyHost,
+            CURLOPT_FOLLOWLOCATION => $this->followLocation
+        ], $options));
+
+        $this->result = curl_exec($ch);
+
+        if ($this->result === false)
+        {
+            $error = curl_error($ch);
+
+            curl_close($ch);
+
+            throw new Exception($error);
+        }
+
+        curl_close($ch);
+
+        return $this->result;
+    }
+
+    public function jsonQuery($url, $post = null, array $options = [])
+    {
+        if (!array_key_exists(CURLOPT_HTTPHEADER, $options))
+        {
+            $options[CURLOPT_HTTPHEADER] = ['Content-Type: application/json', 'accept: application/json'];
+        }
+
+        $content = $this->query($url, $post, $options);
+
+        return json_decode($content, true);
+    }
+
+    public function imageQuery($url, $post = null, array $options = [])
+    {
+        $content = $this->query($url, $post, $options);
+
+        if (!$content || !($size = getimagesizefromstring($content)))
+        {
+            throw new Exception('File "'  . $url . '" is not an image.');
+        }
+
+        return $content;
+    }
+
+}
